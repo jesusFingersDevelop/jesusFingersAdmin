9ea78ab0374634b82768768e35f46bb825e7c21a
11t518s <11t518s20161759@gmail.com>
2021-10-30 00:35:36 +0900

    feature: initialSettings react with typescript #1

---
+++ README.md
@@ -0,0 +1,10 @@
+# initialReactWithTypescript
+initialReactWithTypescript
+
+### 초기에 CRA없이 해당 파일을 클론해서 사용하면 된다.
+
+수정해야할 사항
+
+1. package.json에 name수정
+2. index.html수정
+3. 폴더 구조가 달라질 수 있으니 path 설정해주는 것은 tsconfig.json과 webpack.config.ts.모두 수정해줘야함
---
+++ layouts/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Switch, Route, Redirect} from 'react-router-dom';
+import loadable from '@loadable/component'; // code spliting
+
+const App = () => {
+  return <div>코딩힘내라 최봉수....</div>;
+};
+
+export default App;
